3e43e7902d86c16d50ff186c74fdb74ea5d3d9ef
John Castaño <j_alexander12@hotmail.com>
2016-09-01 13:46:40 -0500

    Agregar archivos de proyecto.

---
+++ Koob.Vista/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Koob.Vista.MvcApplication" Language="C#" %>
---
+++ Koob.Vista/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Koob.Vista.Startup))]
+namespace Koob.Vista
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
