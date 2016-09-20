238678d07bec303402bce96e093ccf6f88f5d88b
John <sisasno@gmail.com>
2016-09-20 17:19:04 -0500

    Permisos de rol usuario

---
+++ Koob.Repositorio/FotosRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Koob.Entidades ;
+using dominio = Koob.Dominio;
+namespace Koob.Repositorio
+{
+    public class FotosRepository: RepositorioGenerico<fotos>
+    {
+    }
+}
---
+++ Koob.Vista/Models/Foto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Koob.Vista.Models
+{
+    public class Foto
+    {
+
+        [Required]
+        [Display(Name = "Foto1")]
+        public byte[] fot_foto1 { get; set; }
+        [Display(Name = "Foto2")]
+        public byte[] fot_foto2 { get; set; }
+        [Display(Name = "Foto3")]
+        public byte[] fot_foto3 { get; set; }
+        [Display(Name = "Foto4")]
+        public byte[] fot_foto4 { get; set; }
+        [Display(Name = "Foto5")]
+        public byte[] fot_foto5 { get; set; }
+    }
+}
